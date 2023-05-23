e4a2ca87f4352f996abb74781c75ba87c88fcd68
gonzalezbri <4717bg@gmail.com>
2023-05-22 20:40:01 -0700

    added submit

---
+++ src/ColorBlock.js
@@ -0,0 +1,9 @@
+function ColorBlock(props) {
+    return (
+        <div className="colorBlock" style={{'backgroundColor': props.color}}>
+            <p>{props.color}</p>
+        </div>
+    )
+}
+
+export default ColorBlock
